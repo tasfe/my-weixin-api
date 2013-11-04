25929f56cb2f2dbf34740f028daed46998a77053
Zibin.Dou <Zibin.Dou@localhost>
2013-11-04 10:18:57 +0800

    ?? ??->???????????????????

--- admin/Lib/Action/MemberSalesAction.class.php
+++ admin/Lib/Action/MemberSalesAction.class.php
@@ -4,16 +4,34 @@
  * 会员优惠管理
  * @author Zibin.Dou{zibin_5257@163.com}
  */
-class MemberSalesAction extends CommonAction {  
-    public function index(){
+class MemberSalesAction extends CommonAction {
+
+    public function index() {
+        $this->check_auth('Member/MemberSales');
         parent::index("and type=0");
     }
+
+    public function add() {
+        $this->check_auth('Member/MemberSales/add');
+        parent::add();
+    }
     
-    public function config(){
+    public function insert() {
+        $this->check_auth('Member/MemberSales/add');
+        parent::insert();
+    }
+    
+    public function delete() {
+        $this->check_auth('Member/MemberSales/delete');
+        parent::delete();
+    }
+
+    public function config() {
+        $this->check_auth('Member/config');
         parent::index("and type=1");
     }
-}
 
+}
 ?>
 
 
