96183eabf798ec123d30671680f456152005c01d
Zibin.Dou <Zibin.Dou@localhost>
2013-11-04 10:30:58 +0800

    ?? ??->???????????????

--- admin/Lib/Action/CouponDuihuanAction.class.php
+++ admin/Lib/Action/CouponDuihuanAction.class.php
@@ -7,6 +7,7 @@
 class CouponDuihuanAction extends CommonAction {
     
     public function index() {
+        $this->check_auth('Coupon/CouponDuihuan');
         $M = M(MODULE_NAME);
 
         //处理搜索条件
@@ -51,6 +52,7 @@ class CouponDuihuanAction extends CommonAction {
      * 兑换方法
      */
     public function duihuan() {
+        $this->check_auth('Coupon/CouponDuihuan/duihuan');
         $this->display();
     }
 
@@ -58,6 +60,7 @@ class CouponDuihuanAction extends CommonAction {
      * 优惠券兑换方法
      */
     public function detail() {
+        $this->check_auth('Coupon/CouponDuihuan/duihuan');
         $code = $_POST['code'];
         $M = M();
         $where = " AND a.code like '%" . $code . "%'";
@@ -74,6 +77,7 @@ class CouponDuihuanAction extends CommonAction {
      * 兑换成功方法
      */
     public function duihuan_action() {
+        $this->check_auth('Coupon/CouponDuihuan/duihuan');
         $id = intval($_POST['id']);
         M('coupon_record')->where("id={$id}")->setField("status", 1);
         $info = M("coupon_record")->where("id={$id}")->find();
@@ -88,88 +92,6 @@ class CouponDuihuanAction extends CommonAction {
         );
         M("coupon_duihuan")->add($data);
         $this->display();
-    }
-
-    
-    /**
-     * 插入方法
-     */
-    public function insert() {
-        $M = D(MODULE_NAME);
-        $data = $M->create();
-        if ($data === false) {
-            $this->ajaxReturn(array('data' => 0));  //失败
-        }
-        $M->create_time = time();
-        if (!empty($_POST['filename_imgupload'])) {
-            $pic_url_big = $_POST['big_imgupload'] . $_POST['filename_imgupload'];
-            $M->pic_url = $pic_url;
-        }
-
-        $M->begin_time = empty($M->begin_time) ? 0 : strtotime($M->begin_time);
-        $M->stop_time = empty($M->stop_time) ? 0 : strtotime($M->stop_time);
-        $M->exchange_stop_time = empty($M->exchange_stop_time) ? 0 : strtotime($M->exchange_stop_time);
-
-        $M->remark = nl2br($M->remark);
-        $M->exchange_explain = nl2br($M->exchange_explain);
-
-        $id = $M->add();
-        if ($id !== false) {
-            $this->ajaxReturn(array('data' => 1));  //成功
-        } else {
-            $this->ajaxReturn(array('data' => 0));  //失败
-        }
-    }
-
-    /**
-     * 编辑页面显示方法
-     */
-    public function edit() {
-        $id = intval($_GET['id']);
-        $M = M(MODULE_NAME);
-        $info = $M->where("id=%d", $id)->find();
-
-        $info['begin_time'] = empty($info['begin_time']) ? $info['begin_time'] : get_date_full($info['begin_time']);
-        $info['stop_time'] = empty($info['stop_time']) ? $info['stop_time'] : get_date_full($info['stop_time']);
-        $info['award_stop_time'] = empty($info['award_stop_time']) ? $info['award_stop_time'] : get_date_full($info['award_stop_time']);
-
-        $info['explain'] = br2nl($info['explain']);
-        $info['prize'] = br2nl($info['prize']);
-
-        unset($M);
-        if (empty($info)) {
-            $this->pop = '编辑的信息不存在！';
-        } else {
-            $this->info = $info;
-        }
-        $this->display();
-    }
-
-    /**
-     * 更新方法
-     */
-    public function update() {
-        $id = intval($_POST['id']);
-        $M = D(MODULE_NAME);
-        $data = $M->create();
-
-        $M->begin_time = empty($M->begin_time) ? 0 : strtotime($M->begin_time);
-        $M->stop_time = empty($M->stop_time) ? 0 : strtotime($M->stop_time);
-        $M->award_stop_time = empty($M->award_stop_time) ? 0 : strtotime($M->award_stop_time);
-
-        $M->direction = nl2br($M->direction);
-        $M->activity = nl2br($M->activity);
-        $M->remark = nl2br($M->remark);
-
-        if ($data === FALSE) {
-            $this->ajaxReturn(array('data' => 0));  //失败
-        }
-
-        if ($M->where("id=%d", $id)->save()) {
-            $this->ajaxReturn(array('data' => 1));  //失败
-        } else {
-            $this->ajaxReturn(array('data' => 0));  //失败
-        }
     }
 
 }
