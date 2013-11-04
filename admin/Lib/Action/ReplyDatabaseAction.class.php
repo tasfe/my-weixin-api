635ee34e0ccd338a17730a7329789a3cea5fcfce
Zibin.Dou <Zibin.Dou@localhost>
2013-11-04 09:45:39 +0800

    ?? ??->??????????????

--- admin/Lib/Action/ReplyDatabaseAction.class.php
+++ admin/Lib/Action/ReplyDatabaseAction.class.php
@@ -6,7 +6,35 @@
  * @author <zibin_5257@163.com>lanfengye
  */
 class ReplyDatabaseAction extends CommonAction {
+    public function index() {
+        $this->check_auth('Reply/ReplyDatabase');
+        parent::index();
+    }
     
+    public function add() {
+        $this->check_auth('Reply/ReplyDatabase/add');
+        parent::add();
+    }
+    
+    public function edit() {
+        $this->check_auth('Reply/ReplyDatabase/edit');
+        parent::edit();
+    }
+    
+    public function insert() {
+        $this->check_auth('Reply/ReplyDatabase/add');
+        parent::insert();
+    }
+    
+    public function update() {
+        $this->check_auth('Reply/ReplyDatabase/edit');
+        parent::update();
+    }
+    
+    public function delete() {
+        $this->check_auth('Reply/ReplyDatabase/delete');
+        parent::delete();
+    }
 }
 
 ?>
