5ba1355e72351e335dd8822f2b5713415d368cff
Fengye.Lan <Fengye.Lan@localhost>
2013-11-05 11:11:04 +0800

    ?? ??->??????

--- admin/Lib/Action/ConfigAction.class.php
+++ admin/Lib/Action/ConfigAction.class.php
@@ -6,10 +6,32 @@
  * @author <zibin_5257@163.com>lanfengye
  */
 class ConfigAction extends CommonAction {
+
     function _initialize() {
         parent::_initialize();
         $this->check_auth('Config/sys_con');
     }
+
+    public function index() {
+        $M = M('Config');
+        $conf_list = $M->where("status=1")->order("sort,name")->select();
+        $this->conf_list = $conf_list;
+//        dump($conf_list);
+        $this->display();
+    }
+
+    public function update() {
+        $M = D(MODULE_NAME);
+        $data = $_POST;
+        foreach ($data as $key => $value) {
+            $M->save(array('name'=>$key,'val'=>$value));
+        }
+        unset($M);
+        S('cache_config',NULL);
+
+        $this->ajaxReturn(array('data' => 1));  //成功
+    }
+
 }
 
 ?>
