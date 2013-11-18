50e2c66ed8007b9c46281ab4fc799b2e2135afc8
Zibin.Dou <Zibin.Dou@localhost>
2013-11-18 09:45:54 +0800

    ?? ??????

--- Conf/config.php
+++ Conf/config.php
@@ -8,12 +8,11 @@ return array(
     
     /* 数据库设置 */
     'DB_TYPE' => 'mysql', // 数据库类型
-    //'DB_HOST' => '10.70.99.34', // 服务器地址
-    'DB_HOST' => '192.168.0.88', // 服务器地址
-    'DB_NAME' => 'weixin_api',
+    'DB_HOST' => '127.0.0.1', // 服务器地址
+    'DB_NAME' => 'weixin',
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => 'lanfengye',
-    'DB_PORT' => 3308, // 端口
+    'DB_PORT' => 3306, // 端口 默认端口应为3306
     'DB_PREFIX' => 'lfy_', // 数据库表前缀
     'DB_SUFFIX' => '', // 数据库表后缀
     
