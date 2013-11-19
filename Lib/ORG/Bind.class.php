b2e410a0225b6fb179e018388ee7d8604ae6bc60
Zibin.Dou <Zibin.Dou@localhost>
2013-11-19 10:47:24 +0800

    ?? ??????????????????????????

--- Lib/ORG/Bind.class.php
+++ Lib/ORG/Bind.class.php
@@ -5,14 +5,16 @@
  * @Created on:2013-11-18 16:16:43
  * @author 蓝枫叶{zibin_5257@163.com}
  */
-class Bind{
+class Bind {
 
+    private $msg_info = array(); //微信消息信息
     private $weixin_id, $content; //微信ID,微信回复内容
     private $shuoming = ''; //指令说明
 
-    function __construct($weixin_id, $content) {
-        $this->weixin_id = $weixin_id;
-        $this->content = $content;
+    function __construct($msg_info = '') {
+        $this->msg_info = $msg_info;
+        $this->weixin_id = (string)  $this->msg_info->FromUserName;
+        $this->content = (string)$this->msg_info->Content;
         $this->shuoming = $this->make_control_shuoming();
     }
 
@@ -38,25 +40,29 @@ class Bind{
         if ($matchs[1] == $mobile_bind_secret) {
             $this->bind_list_control();
         } else {
-            $Index=A('Index');
-            $Index->return_text('请核对您的微信管理绑定密码并重新绑定!');
+            import("@.ORG.TextMsg");
+            $TextMsg = new TextMsg($this->msg_info);
+            $TextMsg->return_text('请核对您的微信管理绑定密码并重新绑定!');
         }
-        exit;
     }
-
+    
+    /**
+     * 进行绑定名单写入操作
+     */
     private function bind_list_control() {
-        $Index=A('Index');
+        import("@.ORG.TextMsg");
+        $TextMsg = new TextMsg($this->msg_info);
         $M = M('MobileBindList');
         if ($M->where("weixin_id='{$this->weixin_id}'")->count('id') > 0) {
-            $Index->return_text("您已经绑定过,{$this->shuoming}");
+            $TextMsg->return_text("您已经绑定过,{$this->shuoming}");
         } else {
             $data = array('weixin_id' => $this->weixin_id, 'create_time' => time(), 'name' => $matchs[2]);
             $return_id = $M->add($data);
             unset($M);
             if ($return_id !== FALSE) {
-                $Index->return_text("手机微信管理绑定成功,{$this->shuoming}");
+                $TextMsg->return_text("手机微信管理绑定成功,{$this->shuoming}");
             } else {
-                $Index->return_text('绑定失败,请尝试重新绑定!');
+                $TextMsg->return_text('绑定失败,请尝试重新绑定!');
             }
         }
     }
