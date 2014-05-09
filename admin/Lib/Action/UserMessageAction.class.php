65d35aaad430bd37d5ccfe4445a3861227c7dd9d
Fengye.Lan <Fengye.Lan@localhost>
2014-05-09 15:35:59 +0800

    ?? ????????????????????

--- admin/Lib/Action/UserMessageAction.class.php
+++ admin/Lib/Action/UserMessageAction.class.php
@@ -27,5 +27,18 @@ class UserMessageAction extends CommonAction {
         $this->list = $list;
         $this->display();
     }
+    
+    /**
+     * 查看用户会话消息
+     */
+    public function view(){
+        $this->check_auth("Message/index/view");
+        $id=intval($_GET['id']);
+        $UserMessage=M("UserMessage");
+        $weixin_name=$UserMessage->where("id={$id}")->getField("wx_name");
+        $list=$UserMessage->where("wx_name='{$weixin_name}'")->order("id desc")->select();
+        $this->list=$list;
+        $this->display();
+    }
 
 }
